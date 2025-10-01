78dd6fe8990434c7c282d4c5b98c79f1ed474dce
tinylove244 <24110142@student.hcmute.edu.vn>
2025-10-02 01:59:30 +0700

    Update Algorithm.h

--- Headers/Algorithm.h
+++ Headers/Algorithm.h
@@ -1,1 +1,16 @@
+#pragma once
+#include <vector>
+#include "Graph.h"
+using namespace std;
+
+class Algorithm {
+public:
+    // Dijkstra: tìm đường đi ngắn nhất giữa 2 tập node
+    static vector<int> dijkstra(
+        const Graph& graph,
+        const vector<int>& startNodes,
+        const vector<int>& endNodes
+    ); // trả về danh sách id node trên đường đi ngắn nhất
+    // vector <int> :  0 1 2 4
+};
 
