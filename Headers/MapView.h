5b33c8eb432f99f06247916721f74440ed3b8072
lsnk_ute <nhatkhue2006@gmail.com>
2025-10-01 14:30:43 +0700

    Create MapView.h

---
+++ Headers/MapView.h
@@ -0,0 +1,20 @@
+#ifndef MAPVIEW_H
+#define MAPVIEW_H
+
+#include <QGraphicsView>
+#include <QWheelEvent>
+#include <QKeyEvent>
+
+class MapView : public QGraphicsView {
+    Q_OBJECT
+protected:
+    void wheelEvent(QWheelEvent *event) override;
+    void keyPressEvent(QKeyEvent *event) override;
+    //void mousePressEvent(QMouseEvent *event) override;
+public:
+    MapView(QWidget *parent = nullptr);
+    
+    double minScale = 0.5; // map3 bị kích thước nên chỉnh lại
+};
+
+#endif
